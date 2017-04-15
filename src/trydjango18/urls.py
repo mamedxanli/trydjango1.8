6d2ddebf80cf645743e60cfced7c0e91e443792d
Alek Mammadov <mamedxanli@gmail.com>
2017-04-15 12:08:26 +0200

    commiting changes

--- src/trydjango18/urls.py
+++ src/trydjango18/urls.py
@@ -16,5 +16,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'newsletter.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
