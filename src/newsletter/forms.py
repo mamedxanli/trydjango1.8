d3c3dab4b62bed8911592e8a98382b983be9d4d3
Alek Mammadov <mamedxanli@gmail.com>
2017-04-15 14:39:51 +0200

    video12

--- src/newsletter/forms.py
+++ src/newsletter/forms.py
@@ -7,3 +7,18 @@ class SignUpForm(forms.ModelForm):
         model = SignUp
         fields = ['full_name', 'email']
         #exclude = ['full_name']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_base, provider = email.split("@")
+        domain, extention = provider.split(".")
+        if not domain == "gmail":
+            raise forms.ValidationError('this is not gmail')
+        if not extention == "edu":
+            raise forms.ValidationError('pls use a valid college email')
+        return email
+
+    def clean_full_name(self):
+        full_name = self.cleaned_data.get('full_name')
+        return full_name
+        
